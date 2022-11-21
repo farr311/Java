6b400457da561890f2d5130b18e4b5270329027e
farr311 <40473601+farr311@users.noreply.github.com>
2022-11-21 20:44:16 +0200

    HW method reference solved

--- src/lesson15/homework/Main.java
+++ src/lesson15/homework/Main.java
@@ -1,5 +1,7 @@
 package lesson15.homework;
 
+import java.util.List;
+
 public class Main {
     /*TODO
        Написать интерфейс Класс Button с методом void click(ButtonAction action)
@@ -16,15 +18,32 @@ public class Main {
     private void testButtons() {
         Button b = new Button();
         b.click(() -> System.out.println("Action"));
-        b.click(MyButtonAction::performAction);
-    }
 
+        ButtonAction ba = new ButtonAction() {
+            @Override
+            public void performAction() {
+                System.out.println("Action 4");
+            }
+        };
 
-    class MyButtonAction implements ButtonAction {
+        b.click(ba::performAction);
 
-        @Override
-        public void performAction() {
-            System.out.println("Action 2");
-        }
+        /*() -> System.out.println("g");                         // Нет аргументов, нет возвращаемого значения
+        () -> 5;                                                // Нет аргументов, значение 5 будет возвращено
+        t -> System.out.println(t);                             // 1 аргумент, нет возвращаемого значения
+        (t) -> System.out.println(t);                           // 1 аргумент, нет возвращаемого значения
+        t -> Integer.valueOf(t);                                // 1 аргумент, значение Integer.valueOf(t) будет возвращено
+        (t) -> Integer.valueOf(t);                              // 1 аргумент, значение Integer.valueOf(t) будет возвращено
+        (a, b, c) -> System.out.println(a + b + c);             // 3 аргумента, нет возвращаемого значения
+        (a, b) -> Integer.valueOf(a) + Integer.valueOf(b);      // 2 аргумента, значение суммы будет возвращено
+
+        () -> {                                                 // Если 1 операция, блок не обязателен, если 2+, то он обязателен
+            System.out.println("g");
+            System.out.println("gg");
+
+            for (int i = 0; i < 10; i++) {
+                return 5;
+            }
+        }*/
     }
 }
