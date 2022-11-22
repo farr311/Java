c244cf47583601f18e1a8355596a5c5f4a222089
farr311 <40473601+farr311@users.noreply.github.com>
2022-11-22 22:32:58 +0200

    Consumer task

--- src/lesson16/StreamPractice.java
+++ src/lesson16/StreamPractice.java
@@ -39,10 +39,12 @@ public class StreamPractice {
         System.out.println(oddSumStream(list));*/
 
 
-        System.out.println(countOccurrences(List.of("a", "b", "a", "c", "a", "d", "s", "x", "a"), "a"));
+        /*System.out.println(countOccurrences(List.of("a", "b", "a", "c", "a", "d", "s", "x", "a"), "a"));
 
         System.out.println(findMin(list));
-        System.out.println(findMax(list));
+        System.out.println(findMax(list));*/
+
+        System.out.println(sortReverseUnique(List.of("a1", "b5", "c1", "a2", "b4", "c1", "a1")));
     }
 
     public static int oddSum(List<Integer> list) {
@@ -68,16 +70,21 @@ public class StreamPractice {
 
     //TODO: написать код через стрим, который будет проверять сколько раз word встречается в list
     public static int countOccurrences(List<String> list, String word) {
-
+        return (int) list.stream().filter(word::equals).count();
     }
 
     //TODO: написать код, который с помощью стримов будет искать наименьший элемент
     public static int findMin(List<Integer> list) {
-
+        return list.stream().min(Integer::compareTo).orElse(-1);
     }
 
     //TODO: написать код, который с помощью стримов будет искать наибольший элемент
     public static int findMax(List<Integer> list) {
+        return list.stream().max(Integer::compareTo).orElse(-1);
+    }
+
+    //TODO: написать стрим, который отсортирует коллекцию в обратном порядке и удалит дубликаты
+    public static List<String> sortReverseUnique(List<String> list) {
 
     }
 }
