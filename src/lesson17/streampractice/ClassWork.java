fe557a7110035a3432b696b592d261125e6eef38
farr311 <40473601+farr311@users.noreply.github.com>
2022-11-24 21:20:26 +0200

    Stream Practice ex1

--- src/lesson17/streampractice/ClassWork.java
+++ src/lesson17/streampractice/ClassWork.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 public class ClassWork {
     private static List<Product> productsEx1;
+    private static List<Order> ordersEx2;
 
     public static void main(String[] args) {
         ex1().forEach(System.out::println);
@@ -18,6 +19,13 @@ public class ClassWork {
                 .filter(p -> p.getCategory().equalsIgnoreCase("Books"))
                 .filter(p -> p.getPrice() > 100)
                 .collect(Collectors.toList());
+    }
+
+    //TODO: Obtain a list of orders with products such that at least one of them belongs to category “Baby”
+    private static List<Product> ex2() {
+        fillDataEx2();
+        return ordersEx2.stream()
+                .//...
     }
 
     private static void fillDataEx1() {
@@ -34,5 +42,53 @@ public class ClassWork {
         productsEx1.add(new Product(0, "9", "Books", 200));
         productsEx1.add(new Product(0, "10", "Books", 1200));
         productsEx1.add(new Product(0, "11", "Food", 11200));
+    }
+
+    private static void fillDataEx2() {
+        List<Product> productsEx2 = new ArrayList<>();
+
+        productsEx2.add(new Product(0, "1", "Baby", 200));
+        productsEx2.add(new Product(0, "2", "Food", 30));
+        productsEx2.add(new Product(0, "3", "Food", 200));
+        productsEx2.add(new Product(0, "4", "Baby", 10));
+        productsEx2.add(new Product(0, "5", "Baby", 200));
+        productsEx2.add(new Product(0, "6", "Food", 250));
+        productsEx2.add(new Product(0, "7", "Food", 200));
+        productsEx2.add(new Product(0, "8", "Baby", 10));
+        productsEx2.add(new Product(0, "9", "Baby", 200));
+        productsEx2.add(new Product(0, "10", "Baby", 1200));
+        productsEx2.add(new Product(0, "11", "Food", 11200));
+
+        ordersEx2 = new ArrayList<>();
+
+        Order o1 = new Order();
+        Order o2 = new Order();
+        Order o3 = new Order();
+        Order o4 = new Order();
+
+        o1.setProduct(productsEx2.get(1));
+        o1.setProduct(productsEx2.get(2));
+        o1.setProduct(productsEx2.get(3));
+        o1.setProduct(productsEx2.get(4));
+
+        o2.setProduct(productsEx2.get(1));
+        o2.setProduct(productsEx2.get(9));
+        o2.setProduct(productsEx2.get(4));
+        o2.setProduct(productsEx2.get(7));
+
+        o3.setProduct(productsEx2.get(0));
+        o3.setProduct(productsEx2.get(5));
+        o3.setProduct(productsEx2.get(3));
+        o3.setProduct(productsEx2.get(2));
+
+        o4.setProduct(productsEx2.get(3));
+        o4.setProduct(productsEx2.get(6));
+        o4.setProduct(productsEx2.get(7));
+        o4.setProduct(productsEx2.get(11));
+
+        ordersEx2.add(o1);
+        ordersEx2.add(o2);
+        ordersEx2.add(o3);
+        ordersEx2.add(o4);
     }
 }
