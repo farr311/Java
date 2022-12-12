07d725f330919bde045bd5c40210b3a2678be415
farr311 <40473601+farr311@users.noreply.github.com>
2022-12-12 20:58:59 +0200

    Stream practice write file

---
+++ src/lesson19/FileStreamsPractice.java
@@ -0,0 +1,22 @@
+package lesson19;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class FileStreamsPractice {
+    //TODO
+    // Используя FileInputStream, прочитайте данные из файла A
+    // И с помощью FileOutputStream допишете их в конец файла B
+
+    // File A: ", my name is <ваше имя>"
+    // File B: "Hello world"
+
+    public static void main(String[] args)  {
+        // fis = new FileInputStream("A");
+        // fos = new FileOutputStream("B", true);
+        // Files.copy(fis, fos)
+
+    }
+}
