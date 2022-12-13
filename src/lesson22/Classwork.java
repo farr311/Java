c80abdc22299059ded7c61e3f1ce78e17ab41cd2
farr311 <40473601+farr311@users.noreply.github.com>
2022-12-13 21:15:54 +0200

    Prime numbers PECS

--- src/lesson22/Classwork.java
+++ src/lesson22/Classwork.java
@@ -1,5 +1,7 @@
 package lesson22;
 
+import java.util.List;
+
 public class Classwork {
 
     public static void main(String[] args) {
@@ -9,5 +11,35 @@ public class Classwork {
         // передать во второй метод, который так же будет типизирован типом коллекции
         // Второй метод должен проверять, является ли число простым, а первый метод должен возвращать количество
         // простых чисел в коллекции
+
+        System.out.println(countPrimes(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)));
+    }
+
+    public static <T extends Number> int countPrimes(List<T> list) {
+        int count = 0;
+
+        for (T e : list) {
+            if (isPrime(e)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static <T extends Number> boolean isPrime(T n) {
+        long num = (long) n.doubleValue();
+
+        if (num != n.doubleValue()) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return num != 1;
     }
 }
