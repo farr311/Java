89bff33605dcd953e6729e3514871f3f31f2a9ce
farr311 <40473601+farr311@users.noreply.github.com>
2023-01-12 22:28:41 +0200

    Multithreading

--- src/main/java/lesson27/Sync.java
+++ src/main/java/lesson27/Sync.java
@@ -1,5 +1,10 @@
 package lesson27;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.IntFunction;
+
 public class Sync {
 
     public static void main(String[] args) throws InterruptedException {
@@ -8,7 +13,9 @@ public class Sync {
         for (int i = 0; i < 4; i++) {
             (new Thread(() -> {
                 for (int j = 0; j < 1000; j++) {
-                    inc.increment();
+                    Modifier.setX(Producer.produce());
+                    Modifier.setFunc(FunctionProducer.produce());
+                    inc.increment(Modifier.Modify());
                 }
             })).start();
         }
@@ -21,11 +28,51 @@ public class Sync {
 class Incrementer {
     private int count = 0;
 
-    public void increment() {
-        count++;
+    public void increment(int x) {
+        count += x;
     }
 
     public int getCount() {
         return count;
+    }
+}
+
+class Modifier {
+
+    private static int x;
+    private static IntFunction<Integer> func;
+    public static int Modify() {
+        return func.apply(x);
+    }
+
+    public static void setX(int a) {
+        x = a;
+    }
+
+    public static void setFunc(IntFunction<Integer> func) {
+        Modifier.func = func;
+    }
+}
+
+class FunctionProducer {
+    private static List<IntFunction<Integer>> list = new ArrayList<>();
+
+    static {
+        list.add(x -> ...);
+        list.add(x -> ...);
+        list.add(x -> ...);
+    }
+    private static Random r = new Random();
+
+    public static IntFunction<Integer> produce() {
+        return list.get(r.nextInt(0, 3));
+    }
+}
+
+class Producer {
+    private static Random r = new Random();
+
+    public static int produce() {
+        return r.nextInt(10);
     }
 }
